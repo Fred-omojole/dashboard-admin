daf1a26de5dbbe99698ea8d71860a6db7d3b8342
Fred-Omojole Omoyele <omoyele.fred-omojole@stu.cu.edu.ng>
2024-03-18 11:54:56 +0100

    sidebar-setup

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,9 +8,9 @@ import { links } from "../data/dummy";
 const Sidebar = () => {
   const activeMenu = true;
   const activeLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg hover:light-gray-200  text-white text-md m-2";
   const normalLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg dark:text-gray-200 dark:hover:text-black hover:light-gray text-md text-gray-700 m-2";
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg dark:text-gray-200 dark:hover:text-black hover:light-gray-200 text-md text-gray-700 m-2";
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
@@ -47,7 +47,10 @@ const Sidebar = () => {
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
-                  ></NavLink> //
+                  >
+                    {link.icon}
+                    <span className="capitalize"> {link.name}</span>
+                  </NavLink>
                 ))}
               </div>
             ))}
